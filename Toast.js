c905e2af53d8208c52a3484b1cdb00ba98a4f3fe
Ashim Khan <ashim07khan@gmail.com>
2022-05-20 21:53:09 +0530

    'update'

--- Toast.js
+++ Toast.js
@@ -3,6 +3,7 @@ export default class Toast {
         // Object.entries(options).forEach(([key, value])=> {
         //     this[key] = value;
         // })
+        this.containers = new Set();
        
     }
 
@@ -11,6 +12,49 @@ export default class Toast {
     }
 
     //position, message, duration, type
+    toaster(options){
+        const {position="bottom", message="Hi there Fella!", duration="2000", type="success"} = options;
+        let toastContainer;
+        if(this.containers.has(position)){
+            toastContainer = document.querySelector(`[data-position=${position}]`);
+        }
+        if(!toastContainer){
+            toastContainer = document.createElement('div');
+            toastContainer.classList.add('meme-toast-container');
+            toastContainer.dataset.position = position;
+            this.containers.add(position);
+        }
+        const toast = document.createElement('div');
+        toast.classList.add('meme-toast');
+        toast.classList.add(type);
+        toast.dataset.duration = duration;
+        const pTag = document.createElement('p');
+        pTag.innerText = message;
+        toast.appendChild(pTag);
+
+        toastContainer.appendChild(toast);
+        document.body.appendChild(toastContainer);
+        this.remover(duration);
+    }
+
+    remover(duration){
+        const time = duration;
+        if (time === 'infinite'){
+            return;
+        }
+        setTimeout(()=>{
+            const toasts = container.querySelectorAll('.meme-toast');
+            toasts.forEach((toast)=>{
+                if(toast.dataset.duration === time){
+                    const parent = toast.parentNode;
+                    toast.remove();
+                    if(!parent.hasChildNodes()) {
+                        parent.remove();
+                    }
+                }
+            });
+        }, parseInt(time));
+    }
 
     toast(options) {
         const {position="bottom", message="Hi there Fella!", duration="2000", type="success"} = options;
