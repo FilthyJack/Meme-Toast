d92823fe4ed8d80d3047b222349af01679f9cef5
Ashim Khan <ashim07khan@gmail.com>
2022-05-28 23:24:41 +0530

    'patch'

--- Toast.js
+++ Toast.js
@@ -7,10 +7,6 @@ export default class Toast {
        
     }
 
-    set position(value) {
-        console.log(value);
-    }
-
     //position, message, duration, type
     toaster(options){
         const {position="bottom", message="Hi there Fella!", duration="2000", type="success"} = options;
@@ -31,10 +27,14 @@ export default class Toast {
         const pTag = document.createElement('p');
         pTag.innerText = message;
         toast.appendChild(pTag);
-
+        toast.addEventListener('click', (e) => {
+            toast.remove();
+        });
         toastContainer.appendChild(toast);
         document.body.appendChild(toastContainer);
+        //appendchild is synchronous
         this.remover(duration);
+
     }
 
     remover(duration){
@@ -53,48 +53,6 @@ export default class Toast {
                     }
                 }
             });
-        }, parseInt(time));
-    }
-
-    toast(options) {
-        const {position="bottom", message="Hi there Fella!", duration="2000", type="success"} = options;
-        let toastContainer = document.querySelector('.meme-toast-container');
-        if(toastContainer === undefined || toastContainer === null) {
-            toastContainer = document.createElement('div');
-            toastContainer.classList.add('meme-toast-container');
-        }
-
-        const toast = document.createElement('div');
-        toast.classList.add('meme-toast');
-        toast.classList.add(type);
-        toast.dataset.duration = duration;
-        const pTag = document.createElement('p');
-        pTag.innerText = message;
-        toast.appendChild(pTag);
-        toast.dataset.position = position;
-
-        toastContainer.appendChild(toast);
-        document.body.appendChild(toastContainer);
-        this.remove(duration);
-    }
-
-    remove(duration) {
-        const time = duration;
-        if (time === 'infinite'){
-            return;
-        }
-        setTimeout(()=>{
-            const container = document.querySelector('.meme-toast-container');
-            const toasts = container.querySelectorAll('.meme-toast');
-            toasts.forEach((toast)=>{
-                if(toast.dataset.duration === time){
-                    console.log('here');
-                    toast.remove();
-                }
-            });
-            if(container.querySelectorAll('.meme-toast').length === 0){
-                container.remove();
-            }
         }, parseInt(time));
     }
 }
